c5380529652973143c55d86b33648cc6d12dd9e8
GloinTheDark <sluce@mac.com>
2015-04-21 15:23:35 -0700

    Added Cop Hawks

--- Boid.h
+++ Boid.h
@@ -11,6 +11,7 @@ public:
 	vec2 position = vec2();
 	vec2 velocity = vec2();
 	vec2 newVelocity = vec2();
+	bool offScreen = false;
 
 	const float maxSpeed = 120;
 	const vec2 center = vec2(640, 480) / 2.0f;
@@ -24,25 +25,63 @@ public:
 
 		const int border = 100;
 		const float turningForce = 0.8;
+		offScreen = false;
 		// keep the boids on the screen
 		if (position.x < border) {
 			newVelocity.x += turningForce;
+			offScreen = true;
 		}
 		if (position.x > 640 - border) {
 			newVelocity.x -= turningForce;
+			offScreen = true;
 		}
 		if (position.y < border) {
 			newVelocity.y += turningForce;
+			offScreen = true;
 		}
 
 		if (position.y > 480 - border) {
 			newVelocity.y -= turningForce;
+			offScreen = true;
 		}
 
 
 	}
 
+	void Chase(const Boid & otherBoid){
+		if (offScreen) {
+			return;
+		}
+		vec2 d = otherBoid.position - position;
+		float len = length(d);
+
+		const float eyeSightLimit = 130; // how far the boid can see
+
+		const float chaseForce = 0.9;
+
+		if (len < eyeSightLimit){
+			newVelocity += normalize(d) * chaseForce;
+		}
+	}
+
+	void Evade(const Boid & otherBoid){
+		vec2 d = otherBoid.position - position;
+		float len = length(d);
+
+		const float eyeSightLimit = 100; // how far the boid can see
+
+		const float evadeForce = 1.9;
+
+		if (len < eyeSightLimit){
+			newVelocity -= normalize(d) * evadeForce;
+		}
+	}
+
 	void FlockTogether(const Boid & otherBoid){
+		if (offScreen) {
+			return;
+		}
+
 		vec2 d = otherBoid.position - position;
 		float len = length(d);
 
@@ -75,6 +114,28 @@ public:
 				if (length(otherBoid.velocity) > 0){
 					newVelocity += normalize(otherBoid.velocity) * flockingForce;
 				}
+			}
+		}
+	}
+
+	void HawkFlock(const Boid & otherBoid){
+		if (offScreen) {
+			return;
+		}
+
+		vec2 d = otherBoid.position - position;
+		float len = length(d);
+
+		const float eyeSightLimit = 130; // how far the boid can see
+
+		const float spacingMin = 90; // how far away from each other they want to be
+		const float moveApartForce = 0.9;
+
+		if (len < eyeSightLimit){
+			// boid can see the other boid
+
+			if (len < spacingMin){
+				newVelocity -= normalize(d) * moveApartForce;
 			}
 		}
 	}
--- Flock.h
+++ Flock.h
@@ -26,25 +26,60 @@ public:
 
 			boidList.push_back(boid);
 		}
+		for (int i = 0; i < 2; i++){
+			Boid * boid = new Boid();
+			int x = rand() % 640;
+			int y = rand() % 480;
+
+			boid->position = vec2(x, y);
+
+			hawklist.push_back(boid);
+		}
 	}
 
 	void move(float elapsedTime){
 
-
 		for (auto boid : boidList){
 			boid->PreMove();
+		}
+
+		for (auto hawk : hawklist){
+			hawk->PreMove();
+		}
 
 
-			for (auto boid2: boidList){
+		for (auto hawk : hawklist){
+			for (auto boid2 : boidList){
+				hawk->Chase(*boid2);
+				boid2->Evade(*hawk);
+			}
+
+			for (auto hawk2 : hawklist){
+				if (hawk != hawk2){
+					hawk->HawkFlock(*hawk2);
+				}
+			}
+		}
+
+
+
+		for (auto boid : boidList){
+			for (auto boid2 : boidList){
 				if (boid != boid2){
 					boid->FlockTogether(*boid2);
 				}
 			}
 		}
 
+
+
 		for (auto boid : boidList){
 			boid->Move(elapsedTime);
 		}
+		for (auto boid : hawklist){
+			boid->Move(elapsedTime);
+		}
+
 	}
 
 	void draw()
@@ -57,6 +92,15 @@ public:
 			boidSprite.setRotation(d);
 
 			boidSprite.draw();
+		}
+		for (auto boid : hawklist){
+			copSprite.setPosition(boid->position);
+
+			float angle = atan2(boid->velocity.y, boid->velocity.x);
+			float d = degrees(angle);
+			copSprite.setRotation(d);
+
+			copSprite.draw();
 		}
 	}
 
